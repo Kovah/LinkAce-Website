6f80b899b3d33d58403629b875654952b89ab32a
Kovah <mail@kovah.de>
2020-09-08 00:10:20 +0200

    Add table of contents to docs layouts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,28 @@
-function registerNavbarToggle(){
+function registerNavbarToggle () {
   const $navBarToggle = document.querySelector('.navbar-toggler');
   const $navBarContent = document.querySelector('#la-navbar-content');
 
-  $navBarToggle.addEventListener('click', (event)=>{
+  $navBarToggle.addEventListener('click', (event) => {
     event.preventDefault();
     $navBarContent.classList.toggle('show');
   });
 }
 
+function registerCollapseToggles () {
+  const collapseToggles = document.querySelectorAll('[data-toggle="collapse"]');
+
+  collapseToggles.forEach($toggle => {
+    const $target = document.querySelector($toggle.dataset.target);
+    if ($target) {
+      $toggle.addEventListener('click', () => {
+        $toggle.setAttribute('aria-expanded', $toggle.getAttribute('aria-expanded') === 'false');
+        $target.classList.toggle('show');
+      });
+    }
+  });
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
   registerNavbarToggle();
+  registerCollapseToggles();
 });
